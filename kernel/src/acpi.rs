daa66609832a1d306e377b83bebeecba8a271cff
Firas88Alkhatib <firas88alkhatib@gmail.com>
2023-03-31 00:45:11 +0300

    add proper mapping and unmapping for ACPI handler

--- kernel/src/acpi.rs
+++ kernel/src/acpi.rs
@@ -1,27 +1,28 @@
 use core::ptr::NonNull;
 use acpi::{ AcpiTables, AcpiHandler, PhysicalMapping, platform::interrupt::Apic };
-use x86_64::VirtAddr;
+use x86_64::{ VirtAddr, structures::paging::Page };
+use crate::memory;
 
 #[derive(Clone)]
-pub struct ACPIHandler {
-    physical_memory_offset: u64,
-}
+pub struct ACPIHandler;
 
 impl AcpiHandler for ACPIHandler {
     unsafe fn map_physical_region<T>(&self, physical_address: usize, size: usize) -> PhysicalMapping<Self, T> {
-        let virtual_address = VirtAddr::new((physical_address as u64) + self.physical_memory_offset);
-        PhysicalMapping::new(physical_address, NonNull::new(virtual_address.as_mut_ptr()).unwrap(), size, size, self.clone())
+        let virtual_address = VirtAddr::new(physical_address as u64); // + self.physical_memory_offset);
+        memory::identity_map(physical_address as u64, None);
+        PhysicalMapping::new(physical_address, NonNull::new(virtual_address.as_mut_ptr()).unwrap(), size, size, Self)
     }
 
-    fn unmap_physical_region<T>(_region: &PhysicalMapping<Self, T>) {
-        // println!("ACPI unmap not implemented");
+    fn unmap_physical_region<T>(region: &PhysicalMapping<Self, T>) {
+        let virtual_address = VirtAddr::new(region.virtual_start().as_ptr() as u64);
+        let page: Page = Page::containing_address(virtual_address);
+        memory::unmap(page);
     }
 }
 
 // Root System Description Pointer
-pub fn init(rsdp_addr: u64, physical_memory_offset: u64) -> Apic {
-    let acpi_handler = ACPIHandler { physical_memory_offset };
-    let acpi_tables = unsafe { AcpiTables::from_rsdp(acpi_handler, rsdp_addr as usize).unwrap() };
+pub fn init(rsdp_addr: u64) -> Apic {
+    let acpi_tables = unsafe { AcpiTables::from_rsdp(ACPIHandler, rsdp_addr as usize).expect("Failed to get ACPI Tables") };
     let platform_info = acpi_tables.platform_info().unwrap();
     let processor_info = platform_info.processor_info.expect("Failed to get processor info");
     println!("---------------ACPI---------------");
